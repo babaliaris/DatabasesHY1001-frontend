f7d91dbb87ac2ca1236e262f40ecad73c33560d4
babaliaris <babaliaris.nikos@gmail.com>
2024-01-08 08:06:09 +0200

    change

--- src/core/unilities.ts
+++ src/core/unilities.ts
@@ -1,14 +1,4 @@
 export function getRandomID(): number
 {
-    const uuid:string = crypto.randomUUID();
-
-    let id: number = 0;
-
-    for (var i = 0; i < uuid.length; i++)
-    {
-        id += uuid.charCodeAt(i);
-        id += Math.random() * 10000;
-    }
-
-    return Math.floor(id);
+    return Math.floor( Date.now() );
 }
